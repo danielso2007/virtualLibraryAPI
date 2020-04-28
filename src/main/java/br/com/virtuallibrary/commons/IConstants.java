17cc6a31f7d4d44de7fffb3c983fd83c86d39c02
daniel.oliveira <danielso2007@gmail.com>
2020-04-27 21:47:08 -0300

    refactor: Ajuste simples.

--- src/main/java/br/com/virtuallibrary/commons/IConstants.java
+++ src/main/java/br/com/virtuallibrary/commons/IConstants.java
@@ -37,6 +37,7 @@ public interface IConstants {
 	// MONGODB
 	String AND = "$and";
 	String REGEX_CONTAINS = ".*%s.*";
+
 	String CONTAINS = "contains";
 	String GREATER_THAN = "gt";
 	String GREATER_THAN_OR_EQUAL = "gte";
